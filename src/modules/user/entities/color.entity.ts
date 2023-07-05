d8a4fd2efe2220f87634f739a806efdfb2638155
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2023-07-05 20:27:17 +0530

    error files

--- src/modules/user/entities/color.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column } from 'typeorm';
-import { BaseEntity } from './base-entity';
-
-@Entity({ name: 'color' })
-export class Color extends BaseEntity {
-  @Column()
-  name: string;
-}
